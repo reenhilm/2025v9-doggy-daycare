9fbf5c6a5484ae7fe5ad4e309c661e5c51928c67
Christian Rönnholm <christian@kajal.se>
2025-02-26 16:06:08 +0100

    moved mappings away from layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { DogsProvider } from "@/context/dogs-context";
 import { fetchDogs } from "./actions";
-import { APIDog, Dog } from "@/interfaces/dogs";
+import { Dog } from "@/interfaces/dogs";
+import { mapAPIDogsToDogs } from "@/interfaces/mappings";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -14,23 +15,6 @@ export const metadata: Metadata = {
   title: "Doggy Daycare",
   description: "Created by Christian Rönnholm",
 };
-
-//TODO: Move to Util
-export function mapAPIDogsToDogs(apiDogs: APIDog[]): Dog[] {
-  return apiDogs.map(mapAPIDogToDog);
-}
-
-//TODO: Move to Util
-export function mapAPIDogToDog(apiDog: APIDog): Dog {
-  return {
-    id: apiDog.chipNumber, // Using chipNumber as a unique identifier
-    name: apiDog.name,
-    breed: apiDog.breed, // Since APIDog doesn't have breed info, set a default value
-    owner: `${apiDog.owner.name} ${apiDog.owner.lastName}`,
-    age: apiDog.age,
-    present: apiDog.present,
-  };
-}
 
 export default async function RootLayout({
   children,
---
+++ interfaces/mappings.ts
@@ -0,0 +1,16 @@
+import { APIDog, Dog } from "@/interfaces/dogs";
+
+export function mapAPIDogsToDogs(apiDogs: APIDog[]): Dog[] {
+  return apiDogs.map(mapAPIDogToDog);
+}
+
+export function mapAPIDogToDog(apiDog: APIDog): Dog {
+  return {
+    id: apiDog.chipNumber, // Using chipNumber as a unique identifier
+    name: apiDog.name,
+    breed: apiDog.breed, // Since APIDog doesn't have breed info, set a default value
+    owner: `${apiDog.owner.name} ${apiDog.owner.lastName}`,
+    age: apiDog.age,
+    present: apiDog.present,
+  };
+}
